67435bc1a57bdfd2d252e8b930ff068cded6fc9d
neeilan <neeilans@live.com>
2016-08-15 04:17:01 +0000

    tutorialNumber -> tutorialCode, to align with courseCode

--- models/tutorial.js
+++ models/tutorial.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose'),
 
 var TutorialSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
-    tutorialNumber: { type: String, required: true, trim: true},
+    tutorialCode: { type: String, required: true, trim: true},
     teachingAssistants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     students: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     groups: [{ type: Schema.Types.ObjectId, ref: 'Group' }]
@@ -12,24 +12,24 @@ var TutorialSchema = new mongoose.Schema({
     timestamps: true
 });
 
-TutorialSchema.methods.generateGroups(size){
-    /* (int) -> null
-    Assigns all students in the tutorial to groups of given size
-  */
-  var randomGroups = [];
-  var shuffledStudents = ArrayUtils.shuffle(this.students);
-  var index = 0;
-  while (index < shuffledStudents.length){
-    randomGroups.push(shuffledStudents.slice(0,index+size));
-    index+= size;
-  }
-  // Prevent one student from being left behind
-  if (shuffledStudents.length % size === 1){
-    randomGroups[randomGroups.length - 1].push(shuffledStudents[shuffledStudents.length -1]);
-  }
-  return randomGroups;
-  // OR this.groups = randomGroups;
-  // return this.save();
-}
+// TutorialSchema.methods.generateGroups = function(size) {
+//     /* (int) -> null
+//     Assigns all students in the tutorial to groups of given size
+//   */
+//   var randomGroups = [];
+//   var shuffledStudents = ArrayUtils.shuffle(this.students);
+//   var index = 0;
+//   while (index < shuffledStudents.length){
+//     randomGroups.push(shuffledStudents.slice(0,index+size));
+//     index+= size;
+//   }
+//   // Prevent one student from being left behind
+//   if (shuffledStudents.length % size === 1){
+//     randomGroups[randomGroups.length - 1].push(shuffledStudents[shuffledStudents.length -1]);
+//   }
+//   return randomGroups;
+//   // OR this.groups = randomGroups;
+//   // return this.save();
+// }
 
 module.exports = mongoose.model('tutorial', TutorialSchema);
