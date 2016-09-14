d6546c885120b5c84e636d4075277d818d6247ce
neeilan <neeilans@live.com>
2016-09-14 03:45:15 +0000

    socketio authentication

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ var bodyParser = require('body-parser'),
     morgan = require('morgan'),
     mongoose = require('mongoose'),
     passport = require('passport'),
-    session = require('express-session');
+    session = require('express-session'),
+    MongoStore = require("connect-mongo")(session),
+    passportSocketIo = require('passport.socketio');
 
 var config = require('./config/common'),
     routes = require('./routes');
 
 var app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server);
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
 
 // locals
 app.locals.io = io; // io global
@@ -32,11 +34,11 @@ app.set('port', process.env.PORT || 8000);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-app.use('/', express.static(__dirname + '/public'));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use('/lodash', express.static(__dirname + '/node_modules/lodash'));
 app.use('/socketioclient', express.static(__dirname + '/node_modules/socket.io-client'));
+app.use('/', express.static(__dirname + '/public'));
 
 app.use(morgan('dev'));
 
@@ -45,10 +47,13 @@ app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
 
 app.use(cookieParser());
+
+var sessionStore = new MongoStore({ mongooseConnection : mongoose.connection });
 app.use(session({ 
     secret: config.session.secret, 
-    saveUninitialized: true, 
-    resave: true 
+    saveUninitialized: false, 
+    store : sessionStore,
+    resave: false 
 }));
 app.use(flash());
 
@@ -64,6 +69,8 @@ app.use(function (req, res, next) {
     next();
 });
 
+
+
 app.use('/', routes.guest);
 app.use('/student', routes.student);
 app.use('/admin', routes.admin);
@@ -72,7 +79,46 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500).render('error', { error: err });
 });
 
+
+/////////////////// realtime quiz testing endpoint ------------------
+io.use(passportSocketIo.authorize({
+  key: 'connect.sid',
+  secret: config.session.secret,
+  store: sessionStore,
+  passport: passport,
+  cookieParser: cookieParser
+}));
+
+app.get('/quiz', function (req, res){
+    require('./models').TutorialQuiz.findOne({})
+    // .populate({
+    //     path : 'quiz',
+    //     model : 'Quiz',
+    //     populate : {
+    //         path : 'questions',
+    //         model : 'Question'
+    //     }
+    // })
+    // .exec()
+    .then(function(tutQuiz){
+        res.render('student/start-quiz.ejs', { quiz : tutQuiz } )
+    })
+})
+
+io.on('connection', function(socket){
+    console.log('connection');
+     if (socket.request.user && socket.request.user.logged_in) {
+      console.log(socket.request.user);
+      socket.join('user:' + socket.request.user._id); // Since a user can connect from multiple devices/ports, use socket.io rooms instead of hashmap
+    }
+    else{
+        console.log('socket unauthenticated');
+    }
+})
+//////////////// -------------
+
+
 // start server
-app.listen(app.get('port'), function () {
+http.listen(app.get('port'), function () {
     console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
