ec1e525fc29d6aea52dfbc64aaaf2a148d4301c9
neeilan <neeilans@live.com>
2016-09-18 05:18:16 +0000

    export the response model

--- models/index.js
+++ models/index.js
@@ -6,3 +6,4 @@ exports.Question = require('./question');
 exports.File = require('./file');
 exports.TutorialQuiz = require('./tutorialQuiz');
 exports.Group = require('./group');
+exports.Response = require('./response');
