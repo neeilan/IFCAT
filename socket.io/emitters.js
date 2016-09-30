edc2c6813cbb64325af9169da2447793b37eb469
neeilan <neeilans@live.com>
2016-09-30 20:09:04 +0000

    finally moved socket.io logic to separate files

---
+++ socket.io/emitters.js
@@ -0,0 +1,25 @@
+'use strict'
+module.exports = function(io){
+  
+    function emitToUsers(users, eventName, data){
+        for (let i = 0; i < users.length; i++){
+            let userId = users[i];
+            io.to('user:'+userId).emit(eventName,data);
+        }
+    }
+    
+    function emitToTutorialQuiz(tutorialQuizId, eventName, data) {
+    io.to('tutorialQuiz:'+tutorialQuizId).emit(eventName, data);
+    }
+    
+    function emitToGroup (groupId, eventName, data){
+    io.to('group:'+groupId).emit(eventName, data);
+    }
+    
+    return {
+    emitToUsers : emitToUsers,
+    emitToTutorialQuiz: emitToTutorialQuiz,
+    emitToGroup : emitToGroup,
+    }
+
+}
